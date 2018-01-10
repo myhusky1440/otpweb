44aa0699da701e3f8ec38b2253bc65e5b97c982a
zhaopeng <zhaopeng@ascs.tech>
2018-01-10 09:39:40 +0800

    feat:初始化项目

---
+++ src/components/totp/totp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      基于时间的OTP
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style>
+
+</style>
